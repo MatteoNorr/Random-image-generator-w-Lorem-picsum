e9511fea3d74a3875380420f4d76344f64e4778c
MatteoNorr <matteo.norrito@outlook.com>
2023-06-28 16:25:19 +0200

    feat: button and create folder 'Image section'

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import ImageSection from "./Components/ImageSection";
+import Button from "./Components/Button";
 import "./App.css";
 
 function App() {
   const [image, setImage] = useState(1);
 
+  const onClickRamdomImg = () => setImage(Math.random() * 10);
+
   return (
     <>
       <ImageSection image={image} />
+      <Button setImage={onClickRamdomImg} />
     </>
   );
 }
